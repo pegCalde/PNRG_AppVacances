3c5497715ca458c4c4a044212b5562fad3580683
pegCalde <calderonpeg@gmail.com>
2021-05-08 10:08:18 +0200

    dependency service photo picker

---
+++ AppVacances/Services/IPhotoPickerService.cs
@@ -0,0 +1,13 @@
+?using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AppVacances
+{
+    public interface IPhotoPickerService
+    {
+        Task<Stream> GetImageStreamAsync();
+    }
+}
