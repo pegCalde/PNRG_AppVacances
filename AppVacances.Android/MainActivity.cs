fcc5480babc5cc2b03e826fb81bb5fe10d5206f4
GuillaumeSavin <guisav1996@hotmail.fr>
2021-05-10 18:47:42 +0200

    [mergePeggyPhotoPicker] Add PhotoPicker Mgt

--- AppVacances.Android/MainActivity.cs
+++ AppVacances.Android/MainActivity.cs
@@ -4,12 +4,17 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using System.IO;
+using System.Threading.Tasks;
+using Android.Content;
 
 namespace AppVacances.Droid
 {
     [Activity(Label = "AppVacances", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        internal static MainActivity Instance { get; private set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -17,7 +22,33 @@ namespace AppVacances.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Instance = this;
         }
+
+        public static readonly int PickImageId = 1000; //propriété, méthode et champ pr le picker
+        public TaskCompletionSource<Stream> PickImageTaskCompletionSource { get; set; }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (intent != null))
+                {
+                    Android.Net.Uri uri = intent.Data;
+                    Stream stream = ContentResolver.OpenInputStream(uri);
+
+                    PickImageTaskCompletionSource.SetResult(stream);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- AppVacances/Controls/RoundedImageControl.xaml.cs
+++ AppVacances/Controls/RoundedImageControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
@@ -30,6 +31,20 @@ namespace AppVacances
                 ((RoundedImageControl)bindable).frame.CornerRadius = Convert.ToSingle((double)newValue);
             }
         });
+
+        async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+        {
+            (sender as ImageButton).IsEnabled = false;
+
+            Stream stream = await DependencyService.Get<IPhotoPickerService>().GetImageStreamAsync();
+
+            if (stream != null)
+            {
+                image.Source = ImageSource.FromStream(() => stream);
+            }
+
+            (sender as ImageButton).IsEnabled = true;
+        }
 
         public ImageSource Image
         {
