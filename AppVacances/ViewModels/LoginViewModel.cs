b306d9447c04fa6f8c2c4f3e739c57dc7e3db7c5
pegCalde <calderonpeg@gmail.com>
2021-05-10 09:06:52 +0200

    login username + password

---
+++ AppVacances/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;  
+using System.ComponentModel;  
+using System.Windows.Input;  
+using Xamarin.Forms; 
+
+namespace AppVacances
+{
+       public class LoginViewModel : INotifyPropertyChanged  
+    {  
+        public Action DisplayInvalidLoginPrompt;   
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private string username;  
+        public string Username
+        {  
+            get { return username; }  
+            set  
+            {
+                username = value;  
+                PropertyChanged(this, new PropertyChangedEventArgs("Username"));  
+            }  
+        }  
+        private string password;    
+        public string Password
+        {  
+            get { return password; }  
+            set  
+            {  
+                password= value;  
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));  
+            }  
+        }  
+        public ICommand SubmitCommand { protected set; get; }  
+        public LoginViewModel()  
+        {  
+            SubmitCommand = new Command(OnSubmit);  
+        }  
+        public void OnSubmit()  
+        {  
+            if (username != "example" || password != "secret")  
+            {  
+                DisplayInvalidLoginPrompt();  
+            }  
+        }  
+    }  
+}  
+}
---
+++ AppVacances/Views/LoginPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace AppVacances
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            var vm = new LoginViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+
+            InitializeComponent();
+
+            Username.Completed += (object sender, EventArgs e) =>
+            {
+                Password.Focus();
+            };
+
+            Password.Completed += (object sender, EventArgs e) =>
+            {
+                vm.SubmitCommand.Execute(null);
+            };
+        }
+    }
+}
