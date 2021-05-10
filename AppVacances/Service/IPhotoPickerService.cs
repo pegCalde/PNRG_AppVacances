3683e6946dd3305e9bca3884a78e3a78f789d869
GuillaumeSavin <guisav1996@hotmail.fr>
2021-05-10 18:47:42 +0200

    [mergePeggyPhotoPicker] Add PhotoPicker Mgt

---
+++ AppVacances/Service/IPhotoPickerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AppVacances
+{
+    public interface IPhotoPickerService
+    {
+        Task<Stream> GetImageStreamAsync();
+    }
+}
